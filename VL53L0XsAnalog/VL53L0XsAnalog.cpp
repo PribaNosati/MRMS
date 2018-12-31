e8e7d1fa1f7530d253c9a3a708247496456efb26
Zoran Pribičević <40704132+PribaNosati@users.noreply.github.com>
2018-12-31 19:39:23 +0100

    Add files via upload

---
+++ VL53L0XsAnalog/VL53L0XsAnalog.cpp
@@ -0,0 +1,64 @@
+#include "VL53L0XsAnalog.h"
+  
+/**Add a sensor
+@param pin - Analog pin, input for distance reading.
+*/
+void VL53L0XsAnalog::add(uint8_t pin) {
+	if (nextFree >= MAX_VL53L0XS_ANALOG)
+		error("Too many lidars.");
+	pins[nextFree] = pin;
+	nextFree++;
+}
+
+/**Distance
+@param sensorNumber - Sensor's ordinal number. Each call of function add() assigns an increasing number to the sensor, starting with 0.
+@return - Distance in mm.
+*/
+uint16_t VL53L0XsAnalog::distance(uint8_t sensorNumber){
+	if (sensorNumber >= nextFree)
+		error("Invalid sensor number");
+	const float f = 2000.0 / (float)MAXIMUM_ANALOG_READ;
+	return (uint16_t)(analogRead(pins[sensorNumber]) * f);
+}
+
+/** Print to all serial ports
+@param message
+@param eol - end of line
+*/
+void VL53L0XsAnalog::print(String message, bool eol) {
+	if (eol) {
+		Serial.println(message);
+		if (serial != 0)
+			serial->println(message);
+	}
+	else {
+		Serial.print(message);
+		if (serial != 0)
+			serial->print(message);
+	}
+}
+
+/**Test
+@param breakWhen - A function returning bool, without arguments. If it returns true, the test() will be interrupted.
+*/
+void VL53L0XsAnalog::test(BreakCondition breakWhen) {
+	char buffer[30];
+	while (breakWhen == 0 || !(*breakWhen)()) {
+		for (uint8_t i = 0; i < nextFree; i++) {
+			sprintf(buffer, "%3i mm ", distance(i));
+			print(buffer);
+		}
+		print("", true);
+		delay(200);
+	}
+}
+
+/**Constructor
+@param hardwareSerial - Serial, Serial1, Serial2,... - an optional serial port, for example for Bluetooth communication
+*/
+VL53L0XsAnalog::VL53L0XsAnalog(HardwareSerial * hardwareSerial) {
+	serial = hardwareSerial;
+	nextFree = 0;
+}
+
+VL53L0XsAnalog::~VL53L0XsAnalog(){}
---
+++ VL53L0XsAnalog/VL53L0XsAnalog.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <Arduino.h>
+/**
+Purpose: Reading of STM VL53L0X lidars, analog output(s).
+@author MRMS team
+@version 0.0 2018-12-31
+Licence: You can use this code any way you like.
+*/
+  
+#define MAX_VL53L0XS_ANALOG 8 //Maximum number of sensors. 
+#define MAXIMUM_ANALOG_READ 800 // Change this number according to Your system in order to get exact maximum distance
+
+typedef bool(*BreakCondition)();
+
+class VL53L0XsAnalog
+{
+	int nextFree;
+	uint8_t pins[MAX_VL53L0XS_ANALOG];
+	HardwareSerial * serial; //Additional serial port
+
+	/** Print to all serial ports
+	@param message
+	@param eol - end of line
+	*/
+	void print(String message, bool eol = false);
+
+public:
+	/**Constructor
+	@param hardwareSerial - Serial, Serial1, Serial2,... - an optional serial port, for example for Bluetooth communication
+	*/
+	VL53L0XsAnalog(HardwareSerial * hardwareSerial = 0);
+
+	~VL53L0XsAnalog();
+
+	/**Add a sensor
+	@param pin - Analog pin, input for distance reading.
+	*/
+	void add(uint8_t pin);
+
+	/**Distance
+	@param sensorNumber - Sensor's ordinal number. Each call of function add() assigns an increasing number to the sensor, starting with 0.
+	@return - Distance in mm.
+	*/
+	uint16_t distance(uint8_t sensorNumber = 0);
+
+	/**Test
+	@param breakWhen - A function returning bool, without arguments. If it returns true, the test() will be interrupted.
+	*/
+	void test(BreakCondition breakWhen = 0);
+};
+
+//Declaration of error function. Definition is in Your code.
+void error(String message);
