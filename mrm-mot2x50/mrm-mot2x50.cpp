c2f3960a06573b16ceb81eb1a48a60f7101f17ae
Zoran Pribičević <40704132+PribaNosati@users.noreply.github.com>
2019-11-02 17:59:44 +0100

    Add files via upload

--- mrm-mot2x50/mrm-mot2x50.cpp
+++ mrm-mot2x50/mrm-mot2x50.cpp
@@ -39,6 +39,7 @@ void Mrm_mot2x50::add(bool isReversed, char * deviceName)
 	case 3:
 		canIn = CAN_ID_MOT2X50_0_MOTOR3_IN;
 		canOut = CAN_ID_MOT2X50_0_MOTOR3_OUT;
+		break;
 	case 4:
 		canIn = CAN_ID_MOT2X50_1_MOTOR0_IN;
 		canOut = CAN_ID_MOT2X50_1_MOTOR0_OUT;
